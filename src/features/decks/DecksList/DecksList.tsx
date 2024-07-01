7f60d993bcd33ed62dbf55a8f97dd38e516cc228
Dmitriy <fstpain@gmail.com>
2024-07-02 00:08:57 +0300

    deck item

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,21 +1,21 @@
 import s from './DecksList.module.css'
 import { useEffect } from 'react'
 import { Deck, DecksApi } from '../decks-api.ts'
-import { useAppDispatch } from '../../../app/store.ts'
-import { setDecksAC } from '../decks-reducer.ts'
+import { store, useAppDispatch } from '../../../app/store.ts'
+import { decksReducer, setDecksAC } from '../decks-reducer.ts'
 import { useSelector } from 'react-redux'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
 
 export const DecksList = () => {
   const dispatch = useAppDispatch()
-  // @ts-ignore
-  const decks = useSelector<Deck[]>(state=>state.decksReducer)
+  const decks = useSelector(state=>state.decksReducer.decks)
+  console.log(store.getState())
+  console.log(decks)
   useEffect(()=>{
     DecksApi.fetchDecks()
         .then(res=>dispatch(setDecksAC(res.data.items)))
   }, [dispatch])
 
-  console.log(decks)
   // @ts-ignore
-  return (<ul className={s.list}>{decks.map((d: Deck)=> <DeckItem deck={d}/>)}</ul>)
+  return (<ul className={s.list}>{decks.map((d: Deck)=><DeckItem key={d.id} deck={d}/>)}</ul>)
 }
